8e523481bf346613abf4f0f45806c789fdadc149
jashanj0tsingh <jshn.singh1@gmail.com>
2020-11-18 19:11:46 -0500

    parenthesis_nesting check

--- src/pie4/paranthesis.go
+++ src/pie4/paranthesis.go
@@ -1,3 +1,34 @@
 package main
 
+import "fmt"
 
+// You are given a string that contains left and right parenthesis characters.
+// Write code to determine whether the parentheses are correctly nested.
+// For example, the strings "(())" and "()()" are correctly nested but "(()()" and ")(" are not.
+
+func checkParenthesisNesting(str string) bool {
+
+	var counter = 0
+	// convert string to a slice of runes
+	cs := []rune(str)
+
+	for _, c := range cs {
+		if c == '(' {
+			counter++
+		} else if c == ')' {
+			counter--
+			if counter < 0 { return false }
+		}
+	}
+
+	return false
+}
+
+func main() {
+	balanced := checkParenthesisNesting(")(")
+	if balanced {
+		fmt.Println("the parenthesis are balanced")
+	} else {
+		fmt.Println("the parenthesis are not balanced")
+	}
+}
